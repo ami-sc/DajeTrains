fe3fe5dfa19cb67b75eebf4296f42e39ba97ba94
Matteo-mariotti <mariotti.1936936@studenti.uniroma1.it>
2023-05-27 18:50:29 +0200

    Sidebar indexing is working
    You can now access the value of the navBar in the homepage through the callback

--- app/lib/nav_drawer.dart
+++ app/lib/nav_drawer.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 import 'drawer_header.dart';
 
-class NavDrawer extends StatelessWidget {
+class NavDrawer extends StatefulWidget {
+  final Function(int) indexCallback;
+
+  @override
+  State<NavDrawer> createState() => _NavDrawerState();
+
+  const NavDrawer({
+    required this.indexCallback,
+    super.key,
+  });
+}
+
+class _NavDrawerState extends State<NavDrawer> {
+  int currentIdx = 0;
+
+  void _onLabelTap(int idx) {
+    setState(() {
+      currentIdx = idx;
+      widget.indexCallback(idx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -15,41 +36,43 @@ class NavDrawer extends StatelessWidget {
       )),
     );
   }
-}
 
-Widget MyDrawerList() {
-  return Container(
-    padding: EdgeInsets.only(top: 15.0, right: 30, left: 30),
-    child: Column(children: [
-      // Show the list of menu drawer
-      menuItem(1, "Homepage", true),
-      menuItem(2, "History", false),
-      menuItem(3, "Scan ticket/pass", false),
-      menuItem(4, "Settings", false),
-      menuItem(5, "Need help?", false),
-    ]),
-  );
-}
+  Widget MyDrawerList() {
+    return Container(
+      padding: EdgeInsets.only(top: 15.0, right: 30, left: 30),
+      child: Column(children: [
+        // Show the list of menu drawer
+        menuItem(1, "Homepage", true),
+        menuItem(2, "History", false),
+        menuItem(3, "Scan ticket/pass", false),
+        menuItem(4, "Settings", false),
+        menuItem(5, "Need help?", false),
+      ]),
+    );
+  }
 
-Widget menuItem(int id, String title, bool selected) {
-  return Material(
-      borderRadius: BorderRadius.circular(50),
-      color: selected ? Color(0xFF97ECFF) : Colors.transparent,
-      child: InkWell(
-          onTap: () {},
-          borderRadius: BorderRadius.circular(50),
-          child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Text(
-                    title,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ))
-                ],
-              ))));
+  Widget menuItem(int id, String title, bool selected) {
+    return Material(
+        borderRadius: BorderRadius.circular(50),
+        color: selected ? Color(0xFF97ECFF) : Colors.transparent,
+        child: InkWell(
+            onTap: () {
+              _onLabelTap(id);
+            },
+            borderRadius: BorderRadius.circular(50),
+            child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ))
+                  ],
+                ))));
+  }
 }
 
 enum DrawerSections {
